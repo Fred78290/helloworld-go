ac57f857bb47afb5e2ecdabdb9b40e371f6a788e
fboltz <frederic.boltz@gmail.com>
2021-05-08 17:08:52 +0200

    Update target registry

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 GOARCH=amd64 go build -o out/linux/amd64/helloworld
 GOARCH=arm64 go build -o out/linux/arm64/helloworld
 
-docker buildx build --pull --platform linux/amd64,linux/arm64 --push -t devregistry.aldunelabs.com/helloworld:v1.0.0 .
+docker buildx build --pull --platform linux/amd64,linux/arm64 --push -t fred78290/helloworld:v1.0.0 .
